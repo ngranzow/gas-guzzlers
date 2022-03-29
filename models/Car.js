8637ff7d682af38a5b95cd28430c4e7a9e7edb8c
carolpang <carolpang17@gmail.com>
2022-03-28 20:32:20 -0700

    models updated

---
+++ models/Car.js
@@ -0,0 +1,48 @@
+// sequelize Post model
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// extends the sequelize model to the Post model
+class Car extends Model {}
+
+// build up data structure for post data
+Car.init(
+  {
+    // set id rule
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Model: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    MPG: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    //set user id rule
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "car",
+  }
+);
+
+// Export this page
+module.exports = Car;
