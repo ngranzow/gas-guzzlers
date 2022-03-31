1a447b02bd92841fb0a97d0e29789550dea11400
ngranzow <nate.granzow@gmail.com>
2022-03-30 21:25:04 -0500

    updated api index.js and fixed connection.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 
 router.use('/users', userRoutes);
-router.use('/gas-routes', gasRoutes);
+router.use('/gas', gasRoutes);
+router.use('/car', carRoutes);
+router.use('/commute', commuteRoutes);
 
 module.exports = router;
