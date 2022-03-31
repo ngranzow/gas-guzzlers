bc1d9c278928597f0ee6cf8a80c02a5d7b82378f
ngranzow <nate.granzow@gmail.com>
2022-03-30 21:31:46 -0500

    updated const in index.js api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const gasRoutes = require('./gas-routes');
+const carRoutes = require('./car-routes');
+const commuteRoutes = require('./commute-routes');
 
 router.use('/users', userRoutes);
 router.use('/gas', gasRoutes);
